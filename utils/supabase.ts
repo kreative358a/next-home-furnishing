92b550c8de800b68e1b90ac0cb888284947a8ed1
kreative358 <105795822+kreative358@users.noreply.github.com>
2024-12-24 15:02:30 +0100

    7 commit

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const bucket = 'next-home-furnishing-bucket';
+const bucket = "next-home-furnishing-bucket";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
@@ -13,13 +13,13 @@ export const uploadImage = async (image: File) => {
   const newName = `${timestamp}-${image.name}`;
   const { data } = await supabase.storage
     .from(bucket)
-    .upload(newName, image, { cacheControl: '3600' });
-  if (!data) throw new Error('Image upload failed');
+    .upload(newName, image, { cacheControl: "3600" });
+  if (!data) throw new Error("Image upload failed");
   return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
 
 export const deleteImage = (url: string) => {
-  const imageName = url.split('/').pop();
-  if (!imageName) throw new Error('Invalid URL');
+  const imageName = url.split("/").pop();
+  if (!imageName) throw new Error("Invalid URL");
   return supabase.storage.from(bucket).remove([imageName]);
 };
